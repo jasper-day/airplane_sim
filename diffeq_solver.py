339684918a6d1f8ce8d08dec4371ae00d6873f9b
Niketa <s2283255@ed.ac.uk>
2023-10-11 17:11:13 +0100

    added code to rk_integrate to loop rk_step

--- diffeq_solver.py
+++ diffeq_solver.py
@@ -4,15 +4,23 @@
 # Method: Runge-Kutta
 
 import math 
-
+import numpy as np
 def rk_integrate(U_0, f, t):
     # Write code that integrates U through the time
-    #create an array from 0 to 100s in time step of 0.1 
-    #iterates rk4 step
     """
     Given initial conditions U_0 (state array), and an array of time values,
     returns an array with the state at each time value [U_0, U_1, ... U_n]
     """
+    #time step size
+    dt = 0.1
+    #create an array for the time in tervals of 0.1 from 0s to 100s
+    t = np.linspace(0, 100, (100/dt)+1)
+    #create an empty array for the state array
+    U = np.empty([100,6])
+    U[0] = [0,0,0,0,0,0] #fill in with initial conditions given by root finding
+    #loop rk4_step for every time value
+    for i in range (0,100):
+        U[i+1] = rk4_step(U[i],f,t[i],dt)
     pass
 
 
@@ -41,3 +49,4 @@ def rk4_step(U_n, f, t, dt):
     f_bar_n_1 = f(U_bar_n_1, t + dt)
     # Simpson's rule
     return dt/6 * (f_n + 2 * f_hat_midpoint + 2 * f_twiddle_midpoint + f_bar_n_1)
+
