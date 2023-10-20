26492b919a35d2f5a9b0b000d524a1e4a970decc
Jasper Day <jromanoday@gmail.com>
2023-10-20 15:54:01 +0100

    Simplified root finding function to minimize
    removed some repeated code, DRY followed

--- root_finder.py
+++ root_finder.py
@@ -5,20 +5,20 @@
 
 from airplane_dynamics import find_lift, find_drag, find_moment, find_weight, \
     find_C_L, find_C_D, find_C_M
-from vehicle import C_D_0, C_L_0, C_L_alpha, C_L_delta_E, C_M_0, C_M_alpha, C_M_delta_E, K_CD
+from vehicle import C_M_0, C_M_alpha, C_M_delta_E
 import math
 
 def _delta_E(alpha):
     return - (C_M_0 + C_M_alpha * alpha) / C_M_delta_E
 
 def _C_L(alpha):
-    return C_L_0 + C_L_alpha * alpha + C_L_delta_E * _delta_E(alpha)
+    return find_C_L(alpha, _delta_E(alpha))
 
 def _C_D(alpha):
-    return C_D_0 + K_CD * _C_L(alpha)**2
+    return find_C_D(_C_L(alpha))
 
 def _C_M(alpha):
-    return C_M_0 + C_M_alpha * alpha + C_M_delta_E * _delta_E(alpha)
+    return find_C_M(alpha, _delta_E(alpha))
 
 def minimizing_function(V, gamma):
     "Returns a function of alpha which should be minimized for trim conditions"
