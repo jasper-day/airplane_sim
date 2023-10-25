f0cbc13712a4cf92fa8dbb9a70948411774b7bae
Jasper Day <jromanoday@gmail.com>
2023-10-25 12:32:41 +0100

    Provide basis for curve fitting

--- curve_fit.py
+++ curve_fit.py
@@ -4,5 +4,18 @@
 
 import numpy as np
 import math
+# import data to fit curves
+from aero_table import CD, CL, CM, CL_el, CM_el, alpha, delta_el
+
+def plot_curve_fit(ax, xscatter, yscatter, xline, yline, linelabel=None, xlabel=None, ylabel=None):
+    ax.scatter(xscatter, yscatter, label='Data')
+    ax.plot(xline, yline, color='red', label=linelabel)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    ax.legend()
+
+def fit_curve(xdata, ydata, degree):eeee
+    # fixme!
+    return np.polyfit(xdata,ydata, degree)
 
 
