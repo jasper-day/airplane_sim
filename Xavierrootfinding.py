4cc4c0831f693576bc2a34a803cd5b1ed45366a5
Xavier <s2323392@ac.ed.uk>
2023-11-01 12:45:47 +0000

    ._.

--- Xavierrootfinding.py
+++ Xavierrootfinding.py
@@ -5,7 +5,33 @@ Created on Mon Oct 16 12:25:58 2023
 
 @author: xavieryee
 """
+from airplane_dynamics import find_lift, find_drag, find_moment, find_weight, \
+    find_C_L, find_C_D, find_C_M
+from vehicle import C_M_0, C_M_alpha, C_M_delta_E
 import numpy as np
+import math
+def minimizing_function(V, gamma):
+    """
+    Returns a function of alpha which should be minimized for trim conditions
+
+    Input:
+    V: Velocity (m/s)
+    gamma: flight angle (rad)
+
+    Output:
+    f: function of alpha
+    """
+    W = find_weight()
+    def f(alpha):
+        L = find_lift(V, _C_L(alpha))
+        D = find_drag(V, _C_D(alpha))
+        M = find_moment(V, _C_M(alpha))
+        return -L*math.cos(alpha) - D*math.sin(alpha) + W*math.cos(alpha + gamma)
+    return f
+def 
+W = find_weight()  
+
+
 def _delta_E(alpha):
     return - (C_M_0 + C_M_alpha * alpha) / C_M_delta_E
 
@@ -19,9 +45,12 @@ def _C_M(alpha):
     return find_C_M(alpha, _delta_E(alpha))
 
 
-L = find_lift(V, _C_L(alpha))
-D = find_drag(V, _C_D(alpha)) 
-W = find_weight()
+def f(alpha):
+        L = find_lift(V, _C_L(alpha))
+        D = find_drag(V, _C_D(alpha))
+        M = find_moment(V, _C_M(alpha))
+        return -L*math.cos(alpha) - D*math.sin(alpha) + W*math.cos(alpha + gamma)
+        return f
 
 def equation (alpha,L,D,W,gamma):
     term_1 = find_lift(V, _C_L(alpha)) * np.cos(alpha)
