cae2021e6d669cb569cb71aba2bd4ac69d318c26
Jasper Day <jromanoday@gmail.com>
2023-11-11 16:25:12 +0000

    Fix commands.py to work with new format of command
    All is good (bugs squashed)

--- commands.py
+++ commands.py
@@ -5,8 +5,7 @@ from airplane_dynamics import deg2rad, dU_dt, find_U_0, find_system_parameters
 import numpy as np
 from diffeq_solver import rk4_integrate
 
-# U = 18 # Thomas is the oldest member, born 18 July
-U = 0 # testing
+U = 18 # Thomas is the oldest member, born 18 July
 
 trim_conditions = {
     "1": find_system(100 + U, 0), # steady level flight
@@ -14,34 +13,50 @@ trim_conditions = {
     "3": find_system(100 + U, 0), # steady level flight
 }
 
-def find_command_array(trim_condition, start_time, end_time, dt):
-    t = np.arange(start_time, end_time, dt)
-    delta_el = np.ones_like(t) * trim_condition["delta_el"]
-    thrust = np.ones_like(t) * trim_condition["Thrust"]
-    X = np.array([delta_el, thrust]).T
-    return {"t": t, "X": X}
-
-
-def find_command_step(trim_conditions, time_starts, total_time, dt):
+def find_command_fn(trim_conditions, time_starts, total_time, dt):
     assert(len(trim_conditions) == len(time_starts))
+    # time_starts is monotonically increasing
+    assert(np.all(np.sign(np.diff(time_starts))))
     time_starts.append(total_time)
-    command_arrays = []
-    for i in range(len(trim_conditions)):
-        trim_condition = trim_conditions[i]
-        command_arrays.append(find_command_array(trim_condition, time_starts[i], time_starts[i + 1], dt))
-    t = np.empty([0])
-    X = np.empty([0,2])
-    for command_array in command_arrays:
-        t = np.hstack((t, command_array['t']))
-        X = np.vstack((X, command_array['X']))
-    return {'t': t, 'X': X}
+    X_commands = []
+    def find_trim(t):
+        for i in range(len(trim_conditions)):
+            if time_starts[i] <= t < time_starts[i + 1]:
+                return [trim_conditions[i]["delta_el"], trim_conditions[i]["Thrust"]]
+        raise RuntimeError(f"Out of bounds access at t={t}")
+    return find_trim
 
 def integrate_system(trim_conditions, time_starts, total_time, dt, starting_altitude=1000):
-    command_array = find_command_step(trim_conditions, time_starts, total_time, dt)
+    X = find_command_fn(trim_conditions, time_starts, total_time, dt)
+    t = np.arange(time_starts[0], total_time, 0.1)
     U_0 = find_U_0(trim_conditions[0], starting_altitude)
-    U_integrated = rk4_integrate(dU_dt, U_0, command_array['X'], command_array['t'])
-    command_array["U"] = U_integrated
-    return command_array
+    U_integrated = rk4_integrate(dU_dt, U_0, X, t)
+    return {"X": X, "U_0": U_0, "U": U_integrated, "t": t}
+
+extract_param = lambda name: [find_system_parameters(U)[name] for U in U_i["U"]]
+extract_command = lambda name: [X[0] if name=="delta_el" else X[1] for X in U_i["X"]]
+
+def make_sample_plot(fig, axs):
+    "Plot system results on a 4x2 subplot"
+    axs[0,0].plot(U_i["t"], extract_param("u_B"))
+    axs[0,0].set_ylabel("$u_B$")
+    axs[0,1].plot(U_i["t"], extract_param("w_B"))
+    axs[0,1].set_ylabel("$w_B$")
+    axs[1,0].plot(U_i["t"], extract_param("q"))
+    axs[1,0].set_ylabel("q")
+    axs[1,1].plot(U_i["t"], extract_param("theta (deg)"))
+    axs[1,1].set_ylabel(r'$\theta$')
+    axs[2,0].plot(U_i["t"], extract_param("gamma (deg)"))
+    axs[2,0].set_ylabel(r"$\gamma$")
+    axs[2,1].plot(U_i["t"], extract_param("z_E"))
+    axs[2,1].set_ylabel(r"$z_E$")
+    axs[3,0].plot(U_i["t"], extract_param("alpha (deg)"))
+    axs[3,0].set_ylabel(r"$\alpha$")
+    axs[3,1].plot(U_i["t"], extract_param("altitude"))
+    axs[3,1].set_ylabel("altitude")
+    fig.tight_layout()
+
+
 
 if __name__=="__main__":
     print("Comparison: page 14 of project.pdf")
@@ -54,24 +69,6 @@ if __name__=="__main__":
     Initial system: {pformat(find_system_parameters(U_i['U'][0]))}
     Initial commands: {pformat(trim_conditions['1'])}
     """)
-    extract_param = lambda name: [find_system_parameters(U)[name] for U in U_i["U"]]
-    extract_command = lambda name: [X[0] if name=="delta_el" else X[1] for X in U_i["X"]]
     fig, axs = plt.subplots(4,2)
-    axs[0,0].plot(U_i["t"], extract_param("u_B"))
-    axs[0,0].set_ylabel("$u_B$")
-    axs[0,1].plot(U_i["t"], extract_param("w_B"))
-    axs[0,1].set_ylabel("$w_B$")
-    axs[1,0].plot(U_i["t"], extract_param("q"))
-    axs[1,0].set_ylabel("q")
-    axs[1,1].plot(U_i["t"], extract_param("theta (deg)"))
-    axs[1,1].set_ylabel(r'$\theta$')
-    axs[2,0].plot(U_i["t"], extract_param("gamma (deg)"))
-    axs[2,0].set_ylabel(r"$\gamma$")
-    axs[2,1].plot(U_i["t"], extract_param("z_B"))
-    axs[2,1].set_ylabel(r"$z_B$")
-    axs[3,0].plot(U_i["t"], extract_param("alpha (deg)"))
-    axs[3,0].set_ylabel(r"$\alpha$")
-    axs[3,1].plot(U_i["t"], extract_param("z_E"))
-    axs[3,1].set_ylabel(r"$z_E$")
-    fig.tight_layout()
+    make_sample_plot(fig,axs)
     plt.show()
