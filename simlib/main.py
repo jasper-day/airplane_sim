b751bba5d16cb4f2671c8daf0526536ce4fd487f
Jasper Day <jromanoday@gmail.com>
2023-11-17 19:19:35 +0000

    Add comments to main.py, deleted xavierrootfinding

--- simlib/main.py
+++ simlib/main.py
@@ -28,16 +28,23 @@ from source.command import integrate_system
 from source.dynamics import find_U_0
 
 
-class SimWindow(QMainWindow, Ui_MainWindow):                           
+class SimWindow(QMainWindow, Ui_MainWindow):
+    # Ui_MainWindow is compiled from ui/simulator-mw.ui using pyside6-designer
     def __init__(self, parent = None):
         super(SimWindow, self).__init__(parent)
+        # create the Ui_MainWindow object
         self.setupUi(self)
+        # output plotting (from source.plot)
         self.graph = GraphWidget()
-        self.graph_layout = QHBoxLayout()
-        self.graph_layout.addWidget(self.graph)
+        # add the graph widget to the graph_output widget
+        graph_layout = QHBoxLayout()
+        graph_layout.addWidget(self.graph)
         self.graph_output.setLayout(self.graph_layout)
-        self.trim_list = [] # data for commands
-        self.initial_conditions = {} # data for initial conditions
+        # trim commands are a list(dict)
+        self.trim_list = [] 
+        # initial conditions are a dict
+        self.initial_conditions = {}
+        # callbacks
         self.add_trim_btn.clicked.connect(self.add_trim)
         self.del_prev_trim_btn.clicked.connect(self.remove_last_trim)
         self.clear_trims_btn.clicked.connect(self.clear_trims)
@@ -46,6 +53,7 @@ class SimWindow(QMainWindow, Ui_MainWindow):
         self.exit_btn.clicked.connect(self.mainwindow)
         self.graph_selector.currentIndexChanged.connect(self.update_plot)
     def add_trim(self):
+        # get trim commands
         if self.trim_vel.value() == 0:
             return
         system = find_system(self.trim_vel.value(), np.radians(self.trim_gamma.value()))
@@ -60,39 +68,49 @@ class SimWindow(QMainWindow, Ui_MainWindow):
         self.trim_list.pop()
         self.updateTable()
     def update_initial_conditions(self):
+        # fail early for impossible simulations
         if self.init_vel.value() == 0:
             raise ValueError("Must have nonzero starting velocity")
         if self.total_time.value() == 0:
             raise ValueError("Must have nonzero simulation time")
+        # find_system adds delta_el and thrust
         self.initial_conditions = find_system(self.init_vel.value(), np.radians(self.init_gamma.value()))
+        # add user-defined parameters to the initial_conditions
         self.initial_conditions["altitude"] = self.init_altitude.value()
         self.initial_conditions["q"] = np.radians(self.init_angvel.value())
         self.initial_conditions["t_total"] = self.total_time.value()
         self.initial_conditions["U_0"] = find_U_0(self.initial_conditions)
     def run_simulation(self):
+        # get initial conditions from ui
         self.update_initial_conditions()
+        # actually run the simulation
         self.sim_result = integrate_system(self.initial_conditions, self.trim_list)
         self.graph_selector.clear()
+        # create a list of graphing options
         graph_selector_keys = list(find_state_parameters(self.sim_result["U"][0]).keys())
         graph_selector_keys.sort()
         self.graph_selector.addItems(graph_selector_keys)
         self.update_plot()
     def update_plot(self):
+        # Check for empty selection
+        if self.graph_selector.currentText() == '':
+            return
         self.graph.ax.clear()
+        # plot selected graph
         plot_parameter(self.graph, self.graph_selector.currentText(), self.sim_result["U"], self.sim_result["t"])
         self.graph.view.draw()
     def mainwindow(self):
+        # return to main window
         self.mw = MainWindow()
         self.mw.show()
         self.hide()
     def updateTable(self):
+        # add a new trim condition to the table of trim conditions
         from pprint import pprint
-        pprint(self.trim_list)
         if len(self.trim_list) == 0:
             self.trim_table.clear()
             return
-        keys = list(self.trim_list[0].keys())
-        keys.sort()
+        keys = sorted(list(self.trim_list[0].keys()))
         self.trim_table.setRowCount(len(keys))
         self.trim_table.setColumnCount(len(self.trim_list))
         self.trim_table.setVerticalHeaderLabels(keys)
@@ -106,33 +124,30 @@ class MainWindow(QWidget):
         super().__init__()
         self.setWindowTitle("SimuPlane™ 0.1.0")
         layout = QGridLayout(self)
-        
         self.label = QtWidgets.QLabel(self)
-
         # Integrate QMovie to the label and initiate the GIF
         self.movie = QMovie("simlib/p51_3d.gif")
         self.label.setMovie(self.movie)
         self.movie.start()
-
         self.label.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
         self.label.setStyleSheet("QLabel {background-color: white;}")
-        
         self.button1 = QPushButton("Start", self)
         self.button1.clicked.connect(self.simwin)
         self.button2 = QPushButton("Quit",self)
         self.button2.clicked.connect(sys.exit)
-        
         layout.addWidget(self.label, 0,0)
         layout.addWidget(self.button1,1,0)
         layout.addWidget(self.button2,2,0)
         self.setLayout(layout)
 
     def simwin(self):
+        # go to main simulation window
         self.simwin = SimWindow()
         self.simwin.show()
         self.hide()
 
 if __name__ == "__main__":
+    # start the application when run
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
