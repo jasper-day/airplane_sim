407f7340cbfb76275ebe910bbb9c1444514584be
Pepperpepsi <thomaswang2002@gmail.com>
2023-11-20 14:20:34 +0000

    updated window files

--- simlib/gui/rf_mw_ui.py
+++ simlib/gui/rf_mw_ui.py
@@ -25,13 +25,17 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(710, 731)
+        MainWindow.resize(906, 715)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.groupBox = QGroupBox(self.centralwidget)
         self.groupBox.setObjectName(u"groupBox")
+        font = QFont()
+        font.setFamilies([u"ISOCP_IV50"])
+        font.setPointSize(22)
+        self.groupBox.setFont(font)
         self.verticalLayout = QVBoxLayout(self.groupBox)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.graph_output = QWidget(self.groupBox)
@@ -41,11 +45,16 @@ class Ui_MainWindow(object):
 
         self.graph_selector = QComboBox(self.groupBox)
         self.graph_selector.setObjectName(u"graph_selector")
+        font1 = QFont()
+        font1.setFamilies([u"ISOCP_IV50"])
+        font1.setPointSize(12)
+        self.graph_selector.setFont(font1)
 
         self.verticalLayout.addWidget(self.graph_selector)
 
         self.elapsed_time = QLineEdit(self.groupBox)
         self.elapsed_time.setObjectName(u"elapsed_time")
+        self.elapsed_time.setFont(font1)
         self.elapsed_time.setReadOnly(True)
 
         self.verticalLayout.addWidget(self.elapsed_time)
@@ -57,26 +66,73 @@ class Ui_MainWindow(object):
         self.widget_3.setObjectName(u"widget_3")
         self.verticalLayout_2 = QVBoxLayout(self.widget_3)
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer_2)
+
         self.textBrowser = QTextBrowser(self.widget_3)
         self.textBrowser.setObjectName(u"textBrowser")
+        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.textBrowser.sizePolicy().hasHeightForWidth())
+        self.textBrowser.setSizePolicy(sizePolicy)
+        self.textBrowser.setMaximumSize(QSize(500, 150))
+        font2 = QFont()
+        font2.setFamilies([u"Segoe UI Light"])
+        font2.setPointSize(11)
+        self.textBrowser.setFont(font2)
 
         self.verticalLayout_2.addWidget(self.textBrowser)
 
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_2.addItem(self.verticalSpacer)
+
         self.groupBox_3 = QGroupBox(self.widget_3)
         self.groupBox_3.setObjectName(u"groupBox_3")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.groupBox_3.sizePolicy().hasHeightForWidth())
+        self.groupBox_3.setSizePolicy(sizePolicy1)
+        self.groupBox_3.setMaximumSize(QSize(500, 16777215))
+        font3 = QFont()
+        font3.setFamilies([u"ISOCP_IV50"])
+        font3.setPointSize(16)
+        self.groupBox_3.setFont(font3)
+        self.groupBox_3.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
         self.verticalLayout_4 = QVBoxLayout(self.groupBox_3)
+        self.verticalLayout_4.setSpacing(0)
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(-1, 0, -1, 9)
         self.widget_4 = QWidget(self.groupBox_3)
         self.widget_4.setObjectName(u"widget_4")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.widget_4.sizePolicy().hasHeightForWidth())
+        self.widget_4.setSizePolicy(sizePolicy2)
         self.horizontalLayout_5 = QHBoxLayout(self.widget_4)
         self.horizontalLayout_5.setObjectName(u"horizontalLayout_5")
         self.label_6 = QLabel(self.widget_4)
         self.label_6.setObjectName(u"label_6")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.label_6.sizePolicy().hasHeightForWidth())
+        self.label_6.setSizePolicy(sizePolicy3)
+        self.label_6.setMaximumSize(QSize(16777215, 26))
+        self.label_6.setFont(font1)
 
         self.horizontalLayout_5.addWidget(self.label_6)
 
         self.method_selector = QComboBox(self.widget_4)
         self.method_selector.setObjectName(u"method_selector")
+        sizePolicy3.setHeightForWidth(self.method_selector.sizePolicy().hasHeightForWidth())
+        self.method_selector.setSizePolicy(sizePolicy3)
+        self.method_selector.setMaximumSize(QSize(16777215, 26))
+        self.method_selector.setFont(font1)
 
         self.horizontalLayout_5.addWidget(self.method_selector)
 
@@ -85,20 +141,35 @@ class Ui_MainWindow(object):
 
         self.widget_2 = QWidget(self.groupBox_3)
         self.widget_2.setObjectName(u"widget_2")
+        sizePolicy3.setHeightForWidth(self.widget_2.sizePolicy().hasHeightForWidth())
+        self.widget_2.setSizePolicy(sizePolicy3)
         self.horizontalLayout_4 = QHBoxLayout(self.widget_2)
         self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
         self.label_5 = QLabel(self.widget_2)
         self.label_5.setObjectName(u"label_5")
+        sizePolicy2.setHeightForWidth(self.label_5.sizePolicy().hasHeightForWidth())
+        self.label_5.setSizePolicy(sizePolicy2)
+        self.label_5.setMaximumSize(QSize(16777215, 26))
+        self.label_5.setFont(font1)
 
         self.horizontalLayout_4.addWidget(self.label_5)
 
         self.err_selector = QComboBox(self.widget_2)
         self.err_selector.setObjectName(u"err_selector")
+        sizePolicy3.setHeightForWidth(self.err_selector.sizePolicy().hasHeightForWidth())
+        self.err_selector.setSizePolicy(sizePolicy3)
+        self.err_selector.setMaximumSize(QSize(16777215, 26))
+        self.err_selector.setFont(font1)
 
         self.horizontalLayout_4.addWidget(self.err_selector)
 
         self.err_amt = QLineEdit(self.widget_2)
         self.err_amt.setObjectName(u"err_amt")
+        sizePolicy3.setHeightForWidth(self.err_amt.sizePolicy().hasHeightForWidth())
+        self.err_amt.setSizePolicy(sizePolicy3)
+        self.err_amt.setMinimumSize(QSize(0, 0))
+        self.err_amt.setMaximumSize(QSize(150, 26))
+        self.err_amt.setFont(font1)
 
         self.horizontalLayout_4.addWidget(self.err_amt)
 
@@ -110,17 +181,30 @@ class Ui_MainWindow(object):
 
         self.groupBox_2 = QGroupBox(self.widget_3)
         self.groupBox_2.setObjectName(u"groupBox_2")
+        sizePolicy1.setHeightForWidth(self.groupBox_2.sizePolicy().hasHeightForWidth())
+        self.groupBox_2.setSizePolicy(sizePolicy1)
+        self.groupBox_2.setMaximumSize(QSize(500, 16777215))
+        self.groupBox_2.setFont(font3)
         self.verticalLayout_3 = QVBoxLayout(self.groupBox_2)
         self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.gridLayout_2 = QGridLayout()
         self.gridLayout_2.setObjectName(u"gridLayout_2")
         self.label_3 = QLabel(self.groupBox_2)
         self.label_3.setObjectName(u"label_3")
+        sizePolicy4 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        sizePolicy4.setHorizontalStretch(0)
+        sizePolicy4.setVerticalStretch(0)
+        sizePolicy4.setHeightForWidth(self.label_3.sizePolicy().hasHeightForWidth())
+        self.label_3.setSizePolicy(sizePolicy4)
+        self.label_3.setFont(font1)
 
         self.gridLayout_2.addWidget(self.label_3, 2, 1, 1, 1)
 
         self.gamma_max = QDoubleSpinBox(self.groupBox_2)
         self.gamma_max.setObjectName(u"gamma_max")
+        sizePolicy2.setHeightForWidth(self.gamma_max.sizePolicy().hasHeightForWidth())
+        self.gamma_max.setSizePolicy(sizePolicy2)
+        self.gamma_max.setFont(font1)
         self.gamma_max.setMaximum(50.000000000000000)
         self.gamma_max.setValue(30.000000000000000)
 
@@ -128,6 +212,9 @@ class Ui_MainWindow(object):
 
         self.vel_min = QDoubleSpinBox(self.groupBox_2)
         self.vel_min.setObjectName(u"vel_min")
+        sizePolicy2.setHeightForWidth(self.vel_min.sizePolicy().hasHeightForWidth())
+        self.vel_min.setSizePolicy(sizePolicy2)
+        self.vel_min.setFont(font1)
         self.vel_min.setMinimum(-100.000000000000000)
         self.vel_min.setMaximum(1000.000000000000000)
         self.vel_min.setSingleStep(10.000000000000000)
@@ -137,16 +224,25 @@ class Ui_MainWindow(object):
 
         self.label = QLabel(self.groupBox_2)
         self.label.setObjectName(u"label")
+        sizePolicy4.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy4)
+        self.label.setFont(font1)
 
         self.gridLayout_2.addWidget(self.label, 0, 1, 1, 1)
 
         self.label_2 = QLabel(self.groupBox_2)
         self.label_2.setObjectName(u"label_2")
+        sizePolicy4.setHeightForWidth(self.label_2.sizePolicy().hasHeightForWidth())
+        self.label_2.setSizePolicy(sizePolicy4)
+        self.label_2.setFont(font1)
 
         self.gridLayout_2.addWidget(self.label_2, 1, 1, 1, 1)
 
         self.vel_max = QDoubleSpinBox(self.groupBox_2)
         self.vel_max.setObjectName(u"vel_max")
+        sizePolicy2.setHeightForWidth(self.vel_max.sizePolicy().hasHeightForWidth())
+        self.vel_max.setSizePolicy(sizePolicy2)
+        self.vel_max.setFont(font1)
         self.vel_max.setMinimum(-100.000000000000000)
         self.vel_max.setMaximum(1000.000000000000000)
         self.vel_max.setSingleStep(10.000000000000000)
@@ -156,11 +252,17 @@ class Ui_MainWindow(object):
 
         self.label_4 = QLabel(self.groupBox_2)
         self.label_4.setObjectName(u"label_4")
+        sizePolicy4.setHeightForWidth(self.label_4.sizePolicy().hasHeightForWidth())
+        self.label_4.setSizePolicy(sizePolicy4)
+        self.label_4.setFont(font1)
 
         self.gridLayout_2.addWidget(self.label_4, 3, 1, 1, 1)
 
         self.gamma_min = QDoubleSpinBox(self.groupBox_2)
         self.gamma_min.setObjectName(u"gamma_min")
+        sizePolicy2.setHeightForWidth(self.gamma_min.sizePolicy().hasHeightForWidth())
+        self.gamma_min.setSizePolicy(sizePolicy2)
+        self.gamma_min.setFont(font1)
         self.gamma_min.setMinimum(-50.000000000000000)
         self.gamma_min.setMaximum(0.000000000000000)
         self.gamma_min.setValue(-30.000000000000000)
@@ -174,6 +276,7 @@ class Ui_MainWindow(object):
         self.gridLayout.setObjectName(u"gridLayout")
         self.horizontalSlider = QSlider(self.groupBox_2)
         self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setFont(font1)
         self.horizontalSlider.setMinimum(30)
         self.horizontalSlider.setMaximum(400)
         self.horizontalSlider.setValue(100)
@@ -186,18 +289,21 @@ class Ui_MainWindow(object):
 
         self.fix_vel = QRadioButton(self.groupBox_2)
         self.fix_vel.setObjectName(u"fix_vel")
+        self.fix_vel.setFont(font1)
         self.fix_vel.setChecked(False)
 
         self.gridLayout.addWidget(self.fix_vel, 0, 0, 1, 1)
 
         self.fix_gamma = QRadioButton(self.groupBox_2)
         self.fix_gamma.setObjectName(u"fix_gamma")
+        self.fix_gamma.setFont(font1)
         self.fix_gamma.setChecked(True)
 
         self.gridLayout.addWidget(self.fix_gamma, 1, 0, 1, 1)
 
         self.widget = QWidget(self.groupBox_2)
         self.widget.setObjectName(u"widget")
+        self.widget.setFont(font1)
         self.horizontalLayout_3 = QHBoxLayout(self.widget)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
         self.slider_label = QLabel(self.widget)
@@ -228,6 +334,11 @@ class Ui_MainWindow(object):
 
         self.return_btn = QPushButton(self.widget_3)
         self.return_btn.setObjectName(u"return_btn")
+        sizePolicy.setHeightForWidth(self.return_btn.sizePolicy().hasHeightForWidth())
+        self.return_btn.setSizePolicy(sizePolicy)
+        self.return_btn.setMinimumSize(QSize(0, 30))
+        self.return_btn.setMaximumSize(QSize(500, 16777215))
+        self.return_btn.setFont(font1)
 
         self.verticalLayout_2.addWidget(self.return_btn)
 
@@ -258,20 +369,20 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Outputs", None))
+        self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Output", None))
         self.textBrowser.setHtml(QCoreApplication.translate("MainWindow", u"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "hr { height: 1px; border-width: 0; }\n"
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This section generates plots of trim conditions at different velocities and pitch angles.</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">To use it, simply select"
-                        " which option to fix, then drag the slider.</p>\n"
-"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p>\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">You can also play with the maximum and minimum values for velocity and pitch angle.</p></body></html>", None))
+"</style></head><body style=\" font-family:'Segoe UI Light'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Segoe UI'; font-size:10pt;\">This section generates plots of trim conditions at different velocities and pitch angles.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI'; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin"
+                        "-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Segoe UI'; font-size:10pt;\">To use it, simply select which option to fix, then drag the slider.</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'Segoe UI'; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Segoe UI'; font-size:10pt;\">You can also play with the maximum and minimum values for velocity and pitch angle.</span></p></body></html>", None))
         self.groupBox_3.setTitle(QCoreApplication.translate("MainWindow", u"Simulation Options", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Root Finding Method:", None))
         self.label_5.setText(QCoreApplication.translate("MainWindow", u"Convergence criteria:", None))
--- simlib/gui/t_climb_mwc.py
+++ simlib/gui/t_climb_mwc.py
@@ -1,9 +1,9 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 ################################################################################
 ## Form generated from reading UI file 't_climb_mw.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.5.3
+## Created by: Qt User Interface Compiler version 6.6.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -24,7 +24,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-        MainWindow.resize(950, 650)
+        MainWindow.resize(928, 669)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.horizontalLayout = QHBoxLayout(self.centralwidget)
@@ -32,6 +32,7 @@ class Ui_MainWindow(object):
         self.horizontalLayout.setContentsMargins(-1, -1, -1, 0)
         self.groupBox_2 = QGroupBox(self.centralwidget)
         self.groupBox_2.setObjectName(u"groupBox_2")
+        self.groupBox_2.setMinimumSize(QSize(500, 0))
         font = QFont()
         font.setFamilies([u"ISOCP_IV50"])
         font.setPointSize(22)
@@ -43,10 +44,7 @@ class Ui_MainWindow(object):
         self.graph_output = QWidget(self.groupBox_2)
         self.graph_output.setObjectName(u"graph_output")
         self.graph_output.setMinimumSize(QSize(500, 0))
-        font1 = QFont()
-        font1.setFamilies([u"ISOCP_IV50"])
-        font1.setPointSize(10)
-        self.graph_output.setFont(font1)
+        self.graph_output.setFont(font)
 
         self.verticalLayout_2.addWidget(self.graph_output)
 
@@ -68,22 +66,22 @@ class Ui_MainWindow(object):
         self.verticalLayout_3.setContentsMargins(-1, -1, -1, 0)
         self.textBrowser = QTextBrowser(self.widget)
         self.textBrowser.setObjectName(u"textBrowser")
-        self.textBrowser.setMinimumSize(QSize(330, 0))
-        self.textBrowser.setMaximumSize(QSize(450, 130))
-        font2 = QFont()
-        font2.setFamilies([u"SansSerif"])
-        font2.setPointSize(11)
-        self.textBrowser.setFont(font2)
+        self.textBrowser.setMinimumSize(QSize(330, 170))
+        self.textBrowser.setMaximumSize(QSize(450, 170))
+        font1 = QFont()
+        font1.setFamilies([u"Segoe UI"])
+        font1.setPointSize(11)
+        self.textBrowser.setFont(font1)
 
         self.verticalLayout_3.addWidget(self.textBrowser, 0, Qt.AlignHCenter)
 
         self.groupBox = QGroupBox(self.widget)
         self.groupBox.setObjectName(u"groupBox")
         self.groupBox.setMinimumSize(QSize(0, 200))
-        font3 = QFont()
-        font3.setFamilies([u"ISOCP_IV50"])
-        font3.setPointSize(16)
-        self.groupBox.setFont(font3)
+        font2 = QFont()
+        font2.setFamilies([u"ISOCP_IV50"])
+        font2.setPointSize(16)
+        self.groupBox.setFont(font2)
         self.verticalLayout = QVBoxLayout(self.groupBox)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout.setContentsMargins(-1, 0, -1, 0)
@@ -92,10 +90,10 @@ class Ui_MainWindow(object):
         self.gridLayout.setVerticalSpacing(10)
         self.velocity = QDoubleSpinBox(self.groupBox)
         self.velocity.setObjectName(u"velocity")
-        font4 = QFont()
-        font4.setFamilies([u"ISOCP_IV50"])
-        font4.setPointSize(12)
-        self.velocity.setFont(font4)
+        font3 = QFont()
+        font3.setFamilies([u"ISOCP_IV50"])
+        font3.setPointSize(12)
+        self.velocity.setFont(font3)
         self.velocity.setMinimum(30.000000000000000)
         self.velocity.setMaximum(400.000000000000000)
         self.velocity.setSingleStep(10.000000000000000)
@@ -105,13 +103,13 @@ class Ui_MainWindow(object):
 
         self.label_3 = QLabel(self.groupBox)
         self.label_3.setObjectName(u"label_3")
-        self.label_3.setFont(font4)
+        self.label_3.setFont(font3)
 
         self.gridLayout.addWidget(self.label_3, 3, 0, 1, 1)
 
         self.final_alt = QDoubleSpinBox(self.groupBox)
         self.final_alt.setObjectName(u"final_alt")
-        self.final_alt.setFont(font4)
+        self.final_alt.setFont(font3)
         self.final_alt.setMinimum(100.000000000000000)
         self.final_alt.setMaximum(10000.000000000000000)
         self.final_alt.setSingleStep(200.000000000000000)
@@ -121,7 +119,7 @@ class Ui_MainWindow(object):
 
         self.climb_angle = QDoubleSpinBox(self.groupBox)
         self.climb_angle.setObjectName(u"climb_angle")
-        self.climb_angle.setFont(font4)
+        self.climb_angle.setFont(font3)
         self.climb_angle.setMinimum(-30.000000000000000)
         self.climb_angle.setMaximum(30.000000000000000)
         self.climb_angle.setValue(2.000000000000000)
@@ -130,19 +128,19 @@ class Ui_MainWindow(object):
 
         self.label = QLabel(self.groupBox)
         self.label.setObjectName(u"label")
-        self.label.setFont(font4)
+        self.label.setFont(font3)
 
         self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
 
         self.label_2 = QLabel(self.groupBox)
         self.label_2.setObjectName(u"label_2")
-        self.label_2.setFont(font4)
+        self.label_2.setFont(font3)
 
         self.gridLayout.addWidget(self.label_2, 1, 0, 1, 1)
 
         self.starting_alt = QDoubleSpinBox(self.groupBox)
         self.starting_alt.setObjectName(u"starting_alt")
-        self.starting_alt.setFont(font4)
+        self.starting_alt.setFont(font3)
         self.starting_alt.setMinimum(100.000000000000000)
         self.starting_alt.setMaximum(10000.000000000000000)
         self.starting_alt.setSingleStep(200.000000000000000)
@@ -152,7 +150,7 @@ class Ui_MainWindow(object):
 
         self.label_4 = QLabel(self.groupBox)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setFont(font4)
+        self.label_4.setFont(font3)
 
         self.gridLayout.addWidget(self.label_4, 2, 0, 1, 1)
 
@@ -166,7 +164,7 @@ class Ui_MainWindow(object):
         self.groupBox_3.setObjectName(u"groupBox_3")
         self.groupBox_3.setMinimumSize(QSize(0, 120))
         self.groupBox_3.setMaximumSize(QSize(16777215, 16777215))
-        self.groupBox_3.setFont(font3)
+        self.groupBox_3.setFont(font2)
         self.verticalLayout_5 = QVBoxLayout(self.groupBox_3)
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
         self.verticalLayout_5.setContentsMargins(-1, 0, -1, 9)
@@ -175,26 +173,26 @@ class Ui_MainWindow(object):
         self.gridLayout_2.setVerticalSpacing(18)
         self.label_6 = QLabel(self.groupBox_3)
         self.label_6.setObjectName(u"label_6")
-        self.label_6.setFont(font4)
+        self.label_6.setFont(font3)
 
         self.gridLayout_2.addWidget(self.label_6, 1, 0, 1, 1)
 
         self.label_5 = QLabel(self.groupBox_3)
         self.label_5.setObjectName(u"label_5")
-        self.label_5.setFont(font4)
+        self.label_5.setFont(font3)
 
         self.gridLayout_2.addWidget(self.label_5, 0, 0, 1, 1)
 
         self.altitude_output = QLineEdit(self.groupBox_3)
         self.altitude_output.setObjectName(u"altitude_output")
-        self.altitude_output.setFont(font4)
+        self.altitude_output.setFont(font3)
         self.altitude_output.setReadOnly(True)
 
         self.gridLayout_2.addWidget(self.altitude_output, 1, 1, 1, 1)
 
         self.t_climb_output = QLineEdit(self.groupBox_3)
         self.t_climb_output.setObjectName(u"t_climb_output")
-        self.t_climb_output.setFont(font4)
+        self.t_climb_output.setFont(font3)
         self.t_climb_output.setReadOnly(True)
 
         self.gridLayout_2.addWidget(self.t_climb_output, 0, 1, 1, 1)
@@ -217,13 +215,13 @@ class Ui_MainWindow(object):
         self.verticalLayout_4.setContentsMargins(-1, -1, -1, 0)
         self.run_btn = QPushButton(self.widget)
         self.run_btn.setObjectName(u"run_btn")
-        self.run_btn.setFont(font4)
+        self.run_btn.setFont(font3)
 
         self.verticalLayout_4.addWidget(self.run_btn)
 
         self.reset_btn = QPushButton(self.widget)
         self.reset_btn.setObjectName(u"reset_btn")
-        self.reset_btn.setFont(font4)
+        self.reset_btn.setFont(font3)
 
         self.verticalLayout_4.addWidget(self.reset_btn)
 
@@ -232,7 +230,7 @@ class Ui_MainWindow(object):
 
         self.return_btn = QPushButton(self.widget)
         self.return_btn.setObjectName(u"return_btn")
-        self.return_btn.setFont(font4)
+        self.return_btn.setFont(font3)
 
         self.verticalLayout_3.addWidget(self.return_btn)
 
@@ -258,9 +256,13 @@ class Ui_MainWindow(object):
 "hr { height: 1px; border-width: 0; }\n"
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
-"</style></head><body style=\" font-family:'SansSerif'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-family:'Segoe UI'; font-size:10pt;\">This section allows you to find the time it takes our airplane to climb from one height to another. To run a simulation, select the appropriate starting and ending heights and the angle at which you wish the plane to climb. The necessary length of time for the plane to fly will be calculated, and a graph of the output displayed.</span></p></bo"
-                        "dy></html>", None))
+"</style></head><body style=\" font-family:'Segoe UI'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">This section allows you to find the time it takes our airplane to climb from one height to another. </span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:"
+                        "0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">To run a simulation, select the appropriate starting and ending heights and the angle at which you wish the plane to climb. </span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">The necessary length of time for the plane to fly will be calculated, and a graph of the output displayed.</span></p></body></html>", None))
         self.groupBox.setTitle(QCoreApplication.translate("MainWindow", u"Simulation Conditions", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"Climb Angle (deg)", None))
         self.label.setText(QCoreApplication.translate("MainWindow", u"Velocity (m/s)", None))
